c896448bff8f7f4b9c6552fa46ffe5fc95afbc56
Niklas Baranski <niklas@baranskis.de>
2023-02-28 10:23:20 +0100

    v1.0

---
+++ src/Hund.java
@@ -0,0 +1,16 @@
+class Hund
+{
+    String name;
+
+    Hund() {
+        String[] namen = {
+                "Bello", "Spike", "Doggy", "Wuffi", "Lassy", "Fips", "Rocky"
+        };
+        name = namen[new java.util.Random().nextInt(namen.length)];
+
+    }
+
+    void bellen() {
+        System.out.println("Wuff, ich bin " + name);
+    }
+}
---
+++ src/Hundestaffel.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+class Hundestaffel {
+
+    private LinkedList<Hund> hunde;
+
+    public Hundestaffel() {
+        hunde = new LinkedList<>();
+        for (int i = 0; i < 20; i++) {
+            hunde.add(new Hund());
+        }
+    }
+
+    public void alleBellen() {
+        for (Hund hund : hunde) {
+            hund.bellen();
+        }
+    }
+
+    public void teilweiseBellen() {
+        for (int i = 0; i < hunde.size(); i += 2) {
+            hunde.get(i).bellen();
+        }
+    }
+
+    public void teilweiseEntfernen() {
+        for (int i = hunde.size() - 1; i >= 0; i -= 2) {
+            hunde.remove(i);
+        }
+    }
+}
